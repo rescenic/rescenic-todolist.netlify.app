843f33b0292b85677bd27367200957ec12a1649a
Muhammad Ridwan Hakim <adminrescen@gmail.com>
2023-01-28 22:12:24 +0700

    v1.7 Add Edit Button

--- src/js/app.js
+++ src/js/app.js
@@ -1,68 +1,66 @@
-// Get form and task list elements
-const form = document.querySelector("#task-form");
-const taskList = document.querySelector("#task-list");
-const taskInput = document.querySelector("#task-input");
+// Ambil referensi dari DOM elements
+const taskInput = document.getElementById('task-input');
+const addTaskButton = document.getElementById('add-task');
+const taskList = document.getElementById('task-list');
 
-// Check for existing tasks in local storage
-let tasks;
-if (localStorage.getItem("tasks")) {
-  tasks = JSON.parse(localStorage.getItem("tasks"));
-} else {
-  tasks = [];
+// Buat array kosong untuk menyimpan tugas
+let tasks = [];
+
+// Periksa apakah ada tugas di penyimpanan lokal dan perbarui array tugas
+if (localStorage.getItem('tasks')) {
+  tasks = JSON.parse(localStorage.getItem('tasks'));
 }
 
-// Add task
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  tasks.push({ task: taskInput.value, done: false });
-  localStorage.setItem("tasks", JSON.stringify(tasks));
-  taskInput.value = "";
-  displayTasks();
+// Render list tugas ke halaman
+function renderTasks() {
+  taskList.innerHTML = '';
+  tasks.forEach((task, index) => {
+    const li = document.createElement('li');
+    li.innerHTML = `
+      ${task}
+      <button class="btn btn-danger btn-sm delete-task" data-index="${index}">Delete</button>
+      <button class="btn btn-info btn-sm edit-task" data-index="${index}">Edit</button>
+    `;
+    taskList.appendChild(li);
+  });
+}
+
+// Tambah sebuah tugas ke list
+function addTask() {
+  const task = taskInput.value;
+  tasks.push(task);
+  taskInput.value = '';
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  renderTasks();
+}
+
+// Hapus sebuah tugas dari list
+function deleteTask(index) {
+  tasks.splice(index, 1);
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  renderTasks();
+}
+
+// Edit sebuah tugas dari list
+function editTask(index) {
+  let newTask = prompt("Enter new task: ");
+  tasks[index] = newTask;
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  renderTasks();
+}
+
+// Tambah event listeners
+addTaskButton.addEventListener('click', addTask);
+taskList.addEventListener('click', (event) => {
+  if (event.target.classList.contains('delete-task')) {
+    const index = event.target.dataset.index;
+    deleteTask(index);
+  }
+  if (event.target.classList.contains('edit-task')) {
+    const index = event.target.dataset.index;
+    editTask(index);
+  }
 });
 
-// Display tasks
-function displayTasks() {
-  let taskListHtml = tasks
-    .map((task, i) => {
-      return `
-        <li class="list-group-item">
-          <input type="checkbox" class="mr-2" ${task.done ? "checked" : ""}>
-          ${task.task}
-          <button class="btn btn-danger btn-sm float-right delete-task" data-index=${i}>Delete</button>
-          <button class="btn btn-info btn-sm float-right mr-2 edit-task" data-index=${i}>Edit</button>
-        </li>
-      `;
-    })
-    .join("");
-  taskList.innerHTML = taskListHtml;
-
-  //delete task
-  let deleteButtons = document.querySelectorAll(".delete-task");
-  deleteButtons.forEach((button) => {
-    button.addEventListener("click", (e) => {
-      let index = e.target.dataset.index;
-      tasks.splice(index, 1);
-      localStorage.setItem("tasks", JSON.stringify(tasks));
-      displayTasks();
-    });
-  });
-
-  //edit task
-  let editButtons = document.querySelectorAll(".edit-task");
-  editButtons.forEach((button) => {
-    button.addEventListener("click", (e) => {
-      let index = e.target.dataset.index;
-      taskInput.value = tasks[index].task;
-      form.addEventListener("submit", (e) => {
-        e.preventDefault();
-        tasks[index].task = taskInput.value;
-        localStorage.setItem("tasks", JSON.stringify(tasks));
-        taskInput.value = "";
-        displayTasks();
-      });
-    });
-  });
-}
-
-//Initial call
-displayTasks();
+// Render list tugas
+renderTasks();
