b5b062090735546f19077c05a784d4767790f2b0
Muhammad Ridwan Hakim <adminrescen@gmail.com>
2023-01-28 21:58:04 +0700

    v1.7-rc

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # rescenic-todolist.netlify.app
 ## Rescenic To-Do List
 #### Changelog:
-versi 1.1 Support localStorage - List Tugas tidak akan hilang ketika refresh/close browser yang sama.<br>
-versi 1.2 Add delete button - Untuk menghapus tugas di list tugas.<br>
+versi 1.1 Support localStorage: List Tugas tidak akan hilang ketika refresh/close browser yang sama.<br>
+versi 1.2 Add Delete Button<br>
 versi 1.3 Try BootStrap via npm<br>
 versi 1.4 Fix paths<br>
 versi 1.5 Add SVG Background & Custom Font<br>
 versi 1.6 Add Credits & Source Code<br>
+versi 1.7 Add Add Edit Button
--- src/js/app.js
+++ src/js/app.js
@@ -1,50 +1,69 @@
-const addTask = document.getElementById("add-task");
-const task = document.getElementById("task");
-const list = document.getElementById("list");
+// Get form and task list elements
+const form = document.querySelector("#task-form");
+const taskList = document.querySelector("#task-list");
+const taskInput = document.querySelector("#task-input");
 
-// Mengambil list tugas dari local storage
-let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+// Check for existing tasks in local storage
+let tasks;
+if (localStorage.getItem("tasks")) {
+  tasks = JSON.parse(localStorage.getItem("tasks"));
+} else {
+  tasks = [];
+}
 
-// Fungsi untuk memuat list tugas yang sudah ada
-function loadTasks() {
-  tasks.forEach(function(task, index) {
-    const li = document.createElement("li");
-    li.classList.add("list-group-item");
-    li.appendChild(document.createTextNode(task));
-    const delBtn = document.createElement("button");
-    delBtn.classList.add("btn", "btn-danger", "delete-btn");
-    delBtn.appendChild(document.createTextNode("X"));
-    li.appendChild(delBtn);
-    list.appendChild(li);
-    delBtn.addEventListener("click", function() {
+// Add task
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  tasks.push({ task: taskInput.value, done: false });
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  taskInput.value = "";
+  displayTasks();
+});
+
+// Display tasks
+function displayTasks() {
+  let taskListHtml = tasks
+    .map((task, i) => {
+      return `
+        <li class="list-group-item">
+          <input type="checkbox" class="mr-2" ${task.done ? "checked" : ""}>
+          ${task.task}
+          <button class="btn btn-danger btn-sm float-right delete-task" data-index=${i}>Delete</button>
+          <button class="btn btn-info btn-sm float-right mr-2 edit-task" data-index=${i}>Edit</button>
+        </li>
+      `;
+    })
+    .join("");
+  taskList.innerHTML = taskListHtml;
+
+  //delete task
+  let deleteButtons = document.querySelectorAll(".delete-task");
+  deleteButtons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      let index = e.target.dataset.index;
       tasks.splice(index, 1);
       localStorage.setItem("tasks", JSON.stringify(tasks));
-      li.remove();
+      displayTasks();
+    });
+  });
+
+  let editButtons = document.querySelectorAll(".edit-task");
+  editButtons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      let index = e.target.dataset.index;
+      taskInput.value = tasks[index].task;
+      form.removeEventListener("submit", addTask);
+      form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        tasks[index].task = taskInput.value;
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+        taskInput.value = "";
+        form.removeEventListener("submit", editTask);
+        form.addEventListener("submit", addTask);
+        displayTasks();
+      });
     });
   });
 }
 
-// Fungsi untuk menambah tugas yang baru
-addTask.addEventListener("click", function(e) {
-  e.preventDefault();
-  if (task.value === "") return;
-  tasks.push(task.value);
-  localStorage.setItem('tasks', JSON.stringify(tasks));
-  const li = document.createElement("li");
-  li.classList.add("list-group-item");
-  li.appendChild(document.createTextNode(task.value));
-  const delBtn = document.createElement("button");
-  delBtn.classList.add("btn", "btn-danger", "delete-btn");
-  delBtn.appendChild(document.createTextNode("X"));
-  li.appendChild(delBtn);
-  list.appendChild(li);
-  task.value = "";
-  delBtn.addEventListener("click", function() {
-    tasks.splice(tasks.indexOf(task.value), 1);
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-    li.remove();
-  });
-});
-
-// Memanggil fungsi loadTasks di halaman saat ini
-loadTasks();
+displayTasks();
