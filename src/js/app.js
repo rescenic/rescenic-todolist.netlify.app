80f69969fd2b1d53e7a66c3013567a1f4b31d9c4
Muhammad Ridwan Hakim <adminrescen@gmail.com>
2023-02-02 08:59:01 +0700

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,21 @@
 // Ambil referensi dari DOM elements
-const taskInput = document.getElementById('task-input');
-const addTaskButton = document.getElementById('add-task');
-const taskList = document.getElementById('task-list');
+const taskInput = document.getElementById("task-input");
+const addTaskButton = document.getElementById("add-task");
+const taskList = document.getElementById("task-list");
 
 // Buat array kosong untuk menyimpan tugas
 let tasks = [];
 
 // Periksa apakah ada tugas di penyimpanan lokal dan perbarui array tugas
-if (localStorage.getItem('tasks')) {
-  tasks = JSON.parse(localStorage.getItem('tasks'));
+if (localStorage.getItem("tasks")) {
+  tasks = JSON.parse(localStorage.getItem("tasks"));
 }
 
 // Render list tugas ke halaman
 function renderTasks() {
-  taskList.innerHTML = '';
+  taskList.innerHTML = "";
   tasks.forEach((task, index) => {
-    const li = document.createElement('li');
+    const li = document.createElement("li");
     li.innerHTML = `
       ${task}
       <button class="btn btn-danger btn-sm delete-task" data-index="${index}">Delete</button>
@@ -29,28 +29,33 @@ function renderTasks() {
 function addTask() {
   const task = taskInput.value;
   tasks.push(task);
-  taskInput.value = '';
-  localStorage.setItem('tasks', JSON.stringify(tasks));
+  taskInput.value = "";
+  localStorage.setItem("tasks", JSON.stringify(tasks));
   renderTasks();
 }
 
 // Hapus sebuah tugas dari list
 function deleteTask(index) {
   tasks.splice(index, 1);
-  localStorage.setItem('tasks', JSON.stringify(tasks));
+  localStorage.setItem("tasks", JSON.stringify(tasks));
   renderTasks();
 }
 
 // Edit sebuah tugas dari list
 function editTask(index) {
-  let newTask = prompt("Enter new task: ");
+  let newTask = prompt("Masukkan tugas baru: ");
   tasks[index] = newTask;
-  localStorage.setItem('tasks', JSON.stringify(tasks));
+  localStorage.setItem("tasks", JSON.stringify(tasks));
   renderTasks();
 }
 
 // Tambah event listeners
 addTaskButton.addEventListener('click', addTask);
+taskInput.addEventListener("keyup", function(event) {
+  if (event.key === "Enter") {
+    addTask();
+  }
+});
 taskList.addEventListener('click', (event) => {
   if (event.target.classList.contains('delete-task')) {
     const index = event.target.dataset.index;
